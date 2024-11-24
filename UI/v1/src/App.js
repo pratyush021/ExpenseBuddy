eeb50bf01910206ecbc386d01195e357bc896ff2
Pratyush Ranjan Bohidar <53327828+pratyush021@users.noreply.github.com>
2024-11-24 23:54:49 +0530

    added react app and navbar

---
+++ UI/v1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ResponsiveAppBar from './components/Drawer';
+
+
+function App() {
+  return (
+    <div>
+      <ResponsiveAppBar></ResponsiveAppBar>
+    </div>
+  );
+}
+
+export default App;
